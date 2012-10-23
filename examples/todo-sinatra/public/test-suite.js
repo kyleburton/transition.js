71ebc5f93fe1c55ae115dc1b90fe672908a44174
Kyle Burton <kburton@relaynetwork.com>
2012-10-22 23:21:54 -0400

    test for adding an item

--- examples/todo-sinatra/public/test-suite.js
+++ examples/todo-sinatra/public/test-suite.js
@@ -15,6 +15,11 @@
     parent.frames.main.confirm = old;
   };
 
+  TodoTestLib.createTestList = function () {
+    Transition.find('input[name="list[name]"]').val('Test');
+    Transition.find('input[type=submit]:visible').click();
+  };
+
 }.call(this));
 
 Transition.addTest({
@@ -51,13 +56,46 @@ Transition.addTest({
     Transition.newState('mainPage', Transition.navigateTo_('/'), {},
       {to: 'createList', pred: Transition.elementExists_('input[name="list[name]"]:visible') }
       ),
-    Transition.newState('createList', 'createList', {},
+    Transition.newState('createList', TodoTestLib.createTestList, {},
       {to: 'success', pred: Transition.elementExists_('li a:contains("Test")') }
+      )
+  ]
+
+});
+
+Transition.addTest({
+  name: 'Add an entry to a List',
+  initialize: function () {
+    console.log(this.get('name') + ' assert initial state: ensure we\'re logged out');
+  },
+
+  states: [
+    Transition.newState('init', Transition.navigateTo_('/'), {},
+      {to: 'deleteTestList', pred: Transition.elementExists_('li a:contains("Test")') },
+      {to: 'mainPage',       pred: Transition.elementNotExists_('li a:contains("Test")') }
+      ),
+    Transition.newState('deleteTestList', TodoTestLib.deleteTestList, {},
+      {to: 'deleteTestList',     pred: Transition.elementExists_('li a:contains("Test")') },
+      {to: 'mainPage',           pred: Transition.elementNotExists_('li a:contains("Test")') }
+      ),
+    Transition.newState('mainPage', Transition.navigateTo_('/'), {},
+      {to: 'createList', pred: Transition.elementExists_('input[name="list[name]"]:visible') }
+      ),
+    Transition.newState('createList', TodoTestLib.createTestList, {},
+      {to: 'addItem', pred: Transition.elementExists_('li a:contains("Test")') }
+      ),
+    Transition.newState('addItem', 'addItem', {},
+      {to: 'success', pred: Transition.elementExists_('li:contains("item")') }
       )
   ],
 
-  createList: function () {
-    Transition.find('input[name="list[name]"]').val('Test');
-    Transition.find('input[type=submit]:visible').click();
+  addItem: function () {
+    Transition.find('input[name="task[name]"]').val('item');
+    Transition.find('form:visible').submit();
   }
 });
+
+// Other tests to create:
+// *) Mark an item as done
+// *) Delete an item
+
